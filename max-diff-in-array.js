0774d110f31c803c76640c29ca5cc7400aed5e50
franzthegit <78289076+franzthegit@users.noreply.github.com>
2021-01-30 23:02:06 -0500

    Create max-diff-in-array.js

---
+++ max-diff-in-array.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+  let maxDiff = -1;
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > min && maxDiff < arr[i] - min) {
+      maxDiff = arr[i] - min;
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return maxDiff;
+}
+
+console.log(solution([4,6,2,2,6,6,1]));
